04b87e0c7a685c7538a768ce73f31b47eb64feef
Pavel Sanatov <psanatov@gmail.com>
2019-10-18 16:47:37 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function parseVideoTitles() {
+  document.querySelectorAll("[data-peer = '291528198']").forEach(e => e.parentNode.removeChild(e));
+
+}
+
+parseVideoTitles();
+
+var targetNode = document.getElementsByTagName('body')[0];
+
+var config = { attributes: true, childList: true, subtree: true };
+var callback = function (mutationsList, observer) {
+  for (var mutation of mutationsList) {
+    if (mutation.type == 'childList') {
+      parseVideoTitles();
+    }
+  }
+};
+
+// Observe
+var observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
